58b2369831270ba50ddd54c966f50246756f6f89
canpok1 <6isstrong@gmail.com>
2020-01-01 12:50:26 +0000

    テスト用ROMでCPUを動かす処理をmainに追記

--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/canpok1/nes-go/pkg/model"
+)
 
 func main() {
-	fmt.Println("HelloWorld!")
+	log.Printf("start")
+
+	var err error
+	defer func() {
+		if err != nil {
+			log.Printf("error: %v", err)
+		}
+		log.Printf("end")
+	}()
+
+	romPath := "./test/roms/hello-world/hello-world.nes"
+
+	rom, err := model.FetchROM(romPath)
+	if err != nil {
+		return
+	}
+
+	cpu := model.NewCPU(rom)
+	err = cpu.Run()
+	if err != nil {
+		return
+	}
 }
