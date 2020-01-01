09a177fea7dc6087b0e60fb107b6a07e797955e6
canpok1 <6isstrong@gmail.com>
2020-01-01 15:38:08 +0000

    CPUバスの書き込み部分を実装

--- pkg/model/cpu.go
+++ pkg/model/cpu.go
@@ -186,19 +186,70 @@ func (c *CPUBus) read(addr uint32) (byte, error) {
 }
 
 // write ...
-func (c *CPUBus) write(addr uint32, data byte) {
-	// TODO 実装
+func (c *CPUBus) write(addr uint32, data byte) error {
+	var err error
+	var target string
+	defer func() {
+		if err != nil {
+			log.Printf("CPUBus.write[addr=%#v] => %#v", addr, err)
+		} else {
+			log.Printf("CPUBus.write[addr=%#v][%v] <= %#v", addr, target, data)
+		}
+	}()
 	// 0x0000～0x07FF	0x0800	WRAM
+	if addr >= 0x0000 && addr <= 0x07FF {
+		target = "WRAM"
+		c.wram[addr] = data
+		return nil
+	} 
+
 	// 0x0800～0x1FFF	-	WRAMのミラー
+	if addr >= 0x0800 && addr <= 0x1FFF {
+		target = "WRAM Mirror"
+		c.wram[addr - 0x0800] = data
+		return nil
+	} 
+
 	// 0x2000～0x2007	0x0008	PPU レジスタ
+	if addr >= 0x2000 && addr <= 0x2007 {
+		target = "PPU Register"
+		c.ppuRegister[addr - 0x2000] = data
+		return nil
+	} 
+
 	// 0x2008～0x3FFF	-	PPUレジスタのミラー
+	if addr >= 0x2008 && addr <= 0x3FFF {
+		target = "PPU Register Mirror"
+		c.ppuRegister[addr - 0x2008] = data
+		return nil
+	} 
+
 	// 0x4000～0x401F	0x0020	APU I/O、PAD
+	if addr >= 0x4000 && addr <= 0x401F {
+		target = "APU I/O, PAD"
+		c.io[addr - 0x4000] = data
+		return nil
+	} 
+
 	// 0x4020～0x5FFF	0x1FE0	拡張ROM
+	if addr >= 0x4000 && addr <= 0x401F {
+		return fmt.Errorf("failed write, cannot write EX ROM; addr: %#v", addr)
+	} 
+
 	// 0x6000～0x7FFF	0x2000	拡張RAM
+	if addr >= 0x6000 && addr <= 0x7FFF {
+		target = "EX RAM"
+		c.exram[addr - 0x6000] = data
+		return nil
+	} 
+
 	// 0x8000～0xBFFF	0x4000	PRG-ROM
 	// 0xC000～0xFFFF	0x4000	PRG-ROM
+	if addr >= 0x8000 && addr <= 0xFFFF {
+		return fmt.Errorf("failed write, cannot write PRG-ROM; addr: %#v", addr)
+	} 
 
-	log.Printf("CPUBus.write[addr=%#v] <= %#v", addr, data)
+	return fmt.Errorf("failed write, addr out of range; addr: %#v", addr)
 }
 
 // Run ...
