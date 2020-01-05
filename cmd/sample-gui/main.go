3c4489dec1c202dfe4c6f60edec8e643a44da1f7
canpok1 <6isstrong@gmail.com>
2020-01-06 00:03:17 +0900

    サンプルの画像を表示するよう修正

--- cmd/sample-gui/main.go
+++ cmd/sample-gui/main.go
@@ -4,14 +4,13 @@ import (
 	"nes-go/pkg/infra"
 	"nes-go/pkg/model"
 	"os"
-	"time"
 
 	"github.com/canpok1/nes-go/pkg/log"
 )
 
 func main() {
 	log.SetOutput(os.Stdout)
-	log.SetLogLevel(log.LevelDebug)
+	log.SetLogLevel(log.LevelInfo)
 
 	log.Debug("========================================")
 	log.Debug("program start")
@@ -58,17 +57,19 @@ func main() {
 
 	go func() {
 		for {
-			cycle, err := cpu.Run()
-			if err != nil {
-				log.Fatal("error: %v", err)
-				break
-			}
+			// cycle, err := cpu.Run()
+			// if err != nil {
+			// 	log.Fatal("error: %v", err)
+			// 	break
+			// }
 
-			imgs, err := ppu.Run(cycle * 3)
-			if err != nil {
-				log.Fatal("error: %v", err)
-				break
-			}
+			// imgs, err := ppu.Run(cycle * 3)
+			// if err != nil {
+			// 	log.Fatal("error: %v", err)
+			// 	break
+			// }
+
+			imgs := makeTestImage()
 
 			if imgs != nil {
 				err = m.Render(imgs)
@@ -78,11 +79,53 @@ func main() {
 				}
 			}
 
-			time.Sleep(time.Millisecond * 100)
+			// time.Sleep(time.Millisecond * 100)
 		}
 
 		return
 	}()
 
 	err = m.Run()
+}
+
+func makeTestImage() [][]model.SpriteImage {
+	imgs := make([][]model.SpriteImage, (model.ResolutionHeight / model.SpriteHeight))
+
+	ci := 0
+	for y := 0; y < (model.ResolutionHeight / model.SpriteHeight); y++ {
+		imgs[y] = make([]model.SpriteImage, (model.ResolutionWidth / model.SpriteWidth))
+		for x := 0; x < (model.ResolutionWidth / model.SpriteWidth); x++ {
+			r := make([][]byte, model.SpriteHeight)
+			g := make([][]byte, model.SpriteHeight)
+			b := make([][]byte, model.SpriteHeight)
+
+			for sy := 0; sy < model.SpriteHeight; sy++ {
+				r[sy] = make([]byte, model.SpriteWidth)
+				g[sy] = make([]byte, model.SpriteWidth)
+				b[sy] = make([]byte, model.SpriteWidth)
+				for sx := 0; sx < model.SpriteWidth; sx++ {
+					switch ci {
+					case 0:
+						r[sy][sx] = 0xFF
+					case 1:
+						g[sy][sx] = 0xFF
+					case 2:
+						b[sy][sx] = 0xFF
+					}
+				}
+			}
+
+			img := model.SpriteImage{
+				R: r,
+				G: g,
+				B: b,
+			}
+
+			imgs[y][x] = img
+
+			ci = (ci + 1) % 3
+		}
+	}
+
+	return imgs
 }
