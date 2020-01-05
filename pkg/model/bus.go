a8f843036a5bf1c625d96919d2992b3e26048c51
canpok1 <6isstrong@gmail.com>
2020-01-05 20:18:23 +0900

    バスの未実装部分を実装

--- pkg/model/bus.go
+++ pkg/model/bus.go
@@ -257,9 +257,7 @@ func (b *Bus) writeByCPU(addr Address, data byte) error {
 }
 
 // readByPPU ...
-func (b *Bus) readByPPU(addr Address) (byte, error) {
-	var data byte
-	var err error
+func (b *Bus) readByPPU(addr Address) (data byte, err error) {
 	var target string
 	log.Debug("Bus.readByPPU[addr=%#v] ...", addr)
 	defer func() {
@@ -279,28 +277,97 @@ func (b *Bus) readByPPU(addr Address) (byte, error) {
 		return data, err
 	}
 
-	// TODO 実装
-	// 0x0000～0x0FFF	0x1000	パターンテーブル0
-	// 0x1000～0x1FFF	0x1000	パターンテーブル1
-	// 0x2000～0x23BF	0x03C0	ネームテーブル0
-	// 0x23C0～0x23FF	0x0040	属性テーブル0
-	// 0x2400～0x27BF	0x03C0	ネームテーブル1
-	// 0x27C0～0x27FF	0x0040	属性テーブル1
-	// 0x2800～0x2BBF	0x03C0	ネームテーブル2
-	// 0x2BC0～0x2BFF	0x0040	属性テーブル2
-	// 0x2C00～0x2FBF	0x03C0	ネームテーブル3
-	// 0x2FC0～0x2FFF	0x0040	属性テーブル3
+	addrTmp := addr
 	// 0x3000～0x3EFF	-	0x2000-0x2EFFのミラー
-	// 0x3F00～0x3F0F	0x0010	バックグラウンドパレット
-	// 0x3F10～0x3F1F	0x0010	スプライトパレット
+	if addr >= 0x3000 && addr <= 0x3EFF {
+		addrTmp = addr - (0x3000 - 0x2000)
+	}
 	// 0x3F20～0x3FFF	-	0x3F00-0x3F1Fのミラー
+	if addr >= 0x3F20 && addr <= 0x3FFF {
+		addrTmp = addr - (0x3F20 - 0x3F00)
+	}
 
-	return data, err
+	// 0x0000～0x0FFF	0x1000	パターンテーブル0
+	if addrTmp >= 0x0000 && addrTmp <= 0x0FFF {
+		data = (*b.charactorROM)[addrTmp]
+		return
+	}
+
+	// 0x1000～0x1FFF	0x1000	パターンテーブル1
+	if addrTmp >= 0x1000 && addrTmp <= 0x1FFF {
+		data = (*b.charactorROM)[addrTmp]
+		return
+	}
+
+	// 0x2000～0x23BF	0x03C0	ネームテーブル0
+	if addrTmp >= 0x2000 && addrTmp <= 0x23BF {
+		data = b.nameTable0[addrTmp-0x2000]
+		return
+	}
+
+	// 0x23C0～0x23FF	0x0040	属性テーブル0
+	if addrTmp >= 0x23C0 && addrTmp <= 0x23FF {
+		data = b.attributeTable0[addrTmp-0x23C0]
+		return
+	}
+
+	// 0x2400～0x27BF	0x03C0	ネームテーブル1
+	if addrTmp >= 0x2400 && addrTmp <= 0x03C0 {
+		data = b.nameTable1[addrTmp-0x2400]
+		return
+	}
+
+	// 0x27C0～0x27FF	0x0040	属性テーブル1
+	if addrTmp >= 0x27C0 && addrTmp <= 0x27FF {
+		data = b.attributeTable1[addrTmp-0x27C0]
+		return
+	}
+
+	// 0x2800～0x2BBF	0x03C0	ネームテーブル2
+	if addrTmp >= 0x2800 && addrTmp <= 0x2BBF {
+		data = b.nameTable2[addrTmp-0x2800]
+		return
+	}
+
+	// 0x2BC0～0x2BFF	0x0040	属性テーブル2
+	if addrTmp >= 0x2BC0 && addrTmp <= 0x0040 {
+		data = b.attributeTable2[addrTmp-0x2BC0]
+		return
+	}
+
+	// 0x2C00～0x2FBF	0x03C0	ネームテーブル3
+	if addrTmp >= 0x2C00 && addrTmp <= 0x2FBF {
+		data = b.nameTable3[addrTmp-0x2C00]
+		return
+	}
+
+	// 0x2FC0～0x2FFF	0x0040	属性テーブル3
+	if addrTmp >= 0x2FC0 && addrTmp <= 0x2FFF {
+		data = b.attributeTable3[addrTmp-0x2FC0]
+		return
+	}
+
+	// 0x3F00～0x3F0F	0x0010	バックグラウンドパレット
+	if addrTmp >= 0x3F00 && addrTmp <= 0x3F0F {
+		pIdx := (addrTmp - 0x3F00) / 4
+		bitIdx := (addrTmp - 0x3F00) % 4
+		data = b.backgroundPalette[pIdx][bitIdx]
+		return
+	}
+	// 0x3F10～0x3F1F	0x0010	スプライトパレット
+	if addrTmp >= 0x3F10 && addrTmp <= 0x3F1F {
+		pIdx := (addrTmp - 0x3F10) / 4
+		bitIdx := (addrTmp - 0x3F10) % 4
+		data = b.spritePalette[pIdx][bitIdx]
+		return
+	}
+
+	err = fmt.Errorf("failed to read by PPU; addr: %v", addr)
+	return
 }
 
 // writeByPPU ...
-func (b *Bus) writeByPPU(addr Address, data byte) error {
-	var err error
+func (b *Bus) writeByPPU(addr Address, data byte) (err error) {
 	var target string
 	log.Debug("Bus.writeByPPU[addr=%#v] ...", addr)
 	defer func() {
@@ -319,24 +386,93 @@ func (b *Bus) writeByPPU(addr Address, data byte) error {
 		err = fmt.Errorf("failed to writeByPPU, bus setup is not completed")
 		return err
 	}
-
-	// TODO 実装
-	// 0x0000～0x0FFF	0x1000	パターンテーブル0
-	// 0x1000～0x1FFF	0x1000	パターンテーブル1
-	// 0x2000～0x23BF	0x03C0	ネームテーブル0
-	// 0x23C0～0x23FF	0x0040	属性テーブル0
-	// 0x2400～0x27BF	0x03C0	ネームテーブル1
-	// 0x27C0～0x27FF	0x0040	属性テーブル1
-	// 0x2800～0x2BBF	0x03C0	ネームテーブル2
-	// 0x2BC0～0x2BFF	0x0040	属性テーブル2
-	// 0x2C00～0x2FBF	0x03C0	ネームテーブル3
-	// 0x2FC0～0x2FFF	0x0040	属性テーブル3
+	addrTmp := addr
 	// 0x3000～0x3EFF	-	0x2000-0x2EFFのミラー
-	// 0x3F00～0x3F0F	0x0010	バックグラウンドパレット
-	// 0x3F10～0x3F1F	0x0010	スプライトパレット
+	if addr >= 0x3000 && addr <= 0x3EFF {
+		addrTmp = addr - (0x3000 - 0x2000)
+	}
 	// 0x3F20～0x3FFF	-	0x3F00-0x3F1Fのミラー
+	if addr >= 0x3F20 && addr <= 0x3FFF {
+		addrTmp = addr - (0x3F20 - 0x3F00)
+	}
 
-	return err
+	// 0x0000～0x0FFF	0x1000	パターンテーブル0
+	if addrTmp >= 0x0000 && addrTmp <= 0x0FFF {
+		(*b.charactorROM)[addrTmp] = data
+		return
+	}
+
+	// 0x1000～0x1FFF	0x1000	パターンテーブル1
+	if addrTmp >= 0x1000 && addrTmp <= 0x1FFF {
+		(*b.charactorROM)[addrTmp] = data
+		return
+	}
+
+	// 0x2000～0x23BF	0x03C0	ネームテーブル0
+	if addrTmp >= 0x2000 && addrTmp <= 0x23BF {
+		b.nameTable0[addrTmp-0x2000] = data
+		return
+	}
+
+	// 0x23C0～0x23FF	0x0040	属性テーブル0
+	if addrTmp >= 0x23C0 && addrTmp <= 0x23FF {
+		b.attributeTable0[addrTmp-0x23C0] = data
+		return
+	}
+
+	// 0x2400～0x27BF	0x03C0	ネームテーブル1
+	if addrTmp >= 0x2400 && addrTmp <= 0x03C0 {
+		b.nameTable1[addrTmp-0x2400] = data
+		return
+	}
+
+	// 0x27C0～0x27FF	0x0040	属性テーブル1
+	if addrTmp >= 0x27C0 && addrTmp <= 0x27FF {
+		b.attributeTable1[addrTmp-0x27C0] = data
+		return
+	}
+
+	// 0x2800～0x2BBF	0x03C0	ネームテーブル2
+	if addrTmp >= 0x2800 && addrTmp <= 0x2BBF {
+		b.nameTable2[addrTmp-0x2800] = data
+		return
+	}
+
+	// 0x2BC0～0x2BFF	0x0040	属性テーブル2
+	if addrTmp >= 0x2BC0 && addrTmp <= 0x0040 {
+		b.attributeTable2[addrTmp-0x2BC0] = data
+		return
+	}
+
+	// 0x2C00～0x2FBF	0x03C0	ネームテーブル3
+	if addrTmp >= 0x2C00 && addrTmp <= 0x2FBF {
+		b.nameTable3[addrTmp-0x2C00] = data
+		return
+	}
+
+	// 0x2FC0～0x2FFF	0x0040	属性テーブル3
+	if addrTmp >= 0x2FC0 && addrTmp <= 0x2FFF {
+		b.attributeTable3[addrTmp-0x2FC0] = data
+		return
+	}
+
+	// 0x3F00～0x3F0F	0x0010	バックグラウンドパレット
+	if addrTmp >= 0x3F00 && addrTmp <= 0x3F0F {
+		pIdx := (addrTmp - 0x3F00) / 4
+		bitIdx := (addrTmp - 0x3F00) % 4
+		b.backgroundPalette[pIdx][bitIdx] = data
+		return
+	}
+	// 0x3F10～0x3F1F	0x0010	スプライトパレット
+	if addrTmp >= 0x3F10 && addrTmp <= 0x3F1F {
+		pIdx := (addrTmp - 0x3F10) / 4
+		bitIdx := (addrTmp - 0x3F10) % 4
+		b.spritePalette[pIdx][bitIdx] = data
+		return
+	}
+
+	err = fmt.Errorf("failed to read by PPU; addr: %v", addr)
+	return
 }
 
 // GetSpriteNo ...
@@ -357,8 +493,8 @@ func (b *Bus) GetSpriteNo(x MonitorX, y MonitorY) (no uint8, err error) {
 		return
 	}
 
-	nameTblX := x / 8
-	nameTblY := y / 8
+	nameTblX := x / SpriteWidth
+	nameTblY := y / SpriteHeight
 	nameTblIndex := uint32(nameTblY)*0x20 + uint32(nameTblX)
 
 	no = b.nameTable0[nameTblIndex]
@@ -369,6 +505,22 @@ func (b *Bus) GetSpriteNo(x MonitorX, y MonitorY) (no uint8, err error) {
 // GetSprite ...
 func (b *Bus) GetSprite(no uint8) *Sprite {
 	return b.charactorROM.GetSprite(no)
+}
+
+// toAttributeTableIndex ...
+func toAttributeTableIndex(x MonitorX, y MonitorY) (i int, err error) {
+	if err = x.Validate(); err != nil {
+		return
+	}
+	if err = y.Validate(); err != nil {
+		return
+	}
+
+	attrTblX := int(x) / (SpriteWidth * 4)
+	attrTblY := int(y) / (SpriteHeight * 4)
+	width := ResolutionWidth / (SpriteWidth * 4)
+	i = attrTblY*width + attrTblX
+	return
 }
 
 // GetPaletteNo ...
@@ -382,21 +534,15 @@ func (b *Bus) GetPaletteNo(x MonitorX, y MonitorY) (no uint8, err error) {
 		}
 	}()
 
-	if err = x.Validate(); err != nil {
+	attrTblIndex, err := toAttributeTableIndex(x, y)
+	if err != nil {
 		return
 	}
-	if err = y.Validate(); err != nil {
-		return
-	}
-
-	attrTblX := x / 8
-	attrTblY := y / 8
-	attrTblIndex := uint32(attrTblY)*0x10 + uint32(attrTblX)
 
 	attribute := b.attributeTable0[attrTblIndex]
 
-	attributeX := (x % 32) / 16
-	attributeY := (y % 32) / 16
+	attributeX := (x % (SpriteWidth * 2)) / SpriteWidth
+	attributeY := (y % (SpriteHeight * 2)) / SpriteHeight
 	attributeIndex := uint32(attributeY)*0x02 + uint32(attributeX)
 
 	no = (attribute & (0x03 << attributeIndex)) >> attributeIndex
